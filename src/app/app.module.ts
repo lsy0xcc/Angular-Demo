81cf7b3fa76787060ab7101bd18024a154211764
lsy0xcc <lsy0xcc@outlook.com>
2022-03-09 23:04:30 +0800

    ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyComComponent } from './my-com/my-com.component';
 import { MyDirectiveComponent } from './my-directive/my-directive.component';
+import { MyFormComponent } from './my-form/my-form.component';
 
 @NgModule({
-  declarations: [AppComponent, MyComComponent, MyDirectiveComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    MyComComponent,
+    MyDirectiveComponent,
+    MyFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
